7d36f3f71d82a72cf6fa5239a96360752039a495
Heffae Johnson <heffae@gmail.com>
2013-11-07 23:02:45 -0700

    added initial software to install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 sudo passwd root
-sudo su -
-apt-get update
-apt-get dist-upgrade
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install vim ubuntu-restricted-extras nvidia-319 indicator-cpufreq unity-tweak-tool libavformat-extra-53 libavcodec-extra-53 libdvdread4 gpointing-device-settings compizconfig-settings-manager compiz-plugins-extra flashplugin-installer chromium
+
+sudo gsettings set com.canonical.desktop.interface scrollbar-mode normal
+sudo /usr/share/doc/libdvdread4/install-css.sh
